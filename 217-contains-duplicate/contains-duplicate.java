27e618e9b3859c8ec90ff1fb623a09e305d0f4f7
dareen-nasreldin <dareennasreldin@gmail.com>
2026-03-13 18:28:49 -0400

    Time: 10 ms (99.50%) | Memory: 97.8 MB (12.82%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,12 +3,8 @@ class Solution {
 
         Set<Integer> duplicate = new HashSet<>();
 
-        for(int i = 0; i < nums.length; i++){
-            if(duplicate.contains(nums[i]))
-            {
-                return true;
-            }
-            duplicate.add(nums[i]);
+        for(int num : nums){
+            if (!duplicate.add(num)) return true;
         }
         return false;
     }
